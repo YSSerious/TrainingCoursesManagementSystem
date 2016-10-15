90233470d1302af98e349ecdc0ed77f4f1dd669e
Алексей <kozakaleksei@gmail.com>
2016-10-15 20:58:07 +0300

    fix

---
+++ src/main/java/ua/ukma/nc/dao/UserDao.java
@@ -0,0 +1,7 @@
+package ua.ukma.nc.dao;
+
+/**
+ * Created by Алексей on 15.10.2016.
+ */
+public interface UserDao {
+}
---
+++ src/main/java/ua/ukma/nc/entity/User.java
@@ -0,0 +1,7 @@
+package ua.ukma.nc.entity;
+
+/**
+ * Created by Алексей on 15.10.2016.
+ */
+public interface User {
+}
