e23ccb9ac127aa1bba3a9563bf7bed670a62fce2
voandrij <andriyvoytenko@MacBook-Pro-Andriy.local>
2016-11-11 15:15:25 +0200

    Certain project draft

---
+++ src/main/java/ua/ukma/nc/controller/CertainProjectController.java
@@ -0,0 +1,27 @@
+package ua.ukma.nc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import ua.ukma.nc.dto.ProjectDto;
+import ua.ukma.nc.dto.UserDto;
+import ua.ukma.nc.service.ProjectService;
+
+public class CertainProjectController {
+	
+	@Autowired
+	private ProjectService projectService;
+	
+	@RequestMapping(value = "/certainProject/{name}", method = RequestMethod.GET)
+	public ModelAndView viewProject(@PathVariable("name") String name) {
+		ModelAndView model = new ModelAndView();
+		
+		ProjectDto prDto = new ProjectDto(projectService.getByName(name));
+		model.addObject("project",prDto);
+		model.setViewName("project");
+		return model;
+	}
+}
