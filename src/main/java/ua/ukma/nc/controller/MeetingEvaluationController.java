1096cf76e750902410df949608dcb94f8576c895
Alex <alex.frankiv13@gmail.com>
2016-11-14 21:40:16 +0200

    set_no_attandance controller and js function added

--- src/main/java/ua/ukma/nc/controller/MeetingEvaluationController.java
+++ src/main/java/ua/ukma/nc/controller/MeetingEvaluationController.java
@@ -1,10 +1,22 @@
 package ua.ukma.nc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import ua.ukma.nc.entity.Meeting;
+import ua.ukma.nc.entity.MeetingReview;
+import ua.ukma.nc.entity.User;
+import ua.ukma.nc.entity.impl.real.MeetingReviewImpl;
+import ua.ukma.nc.service.MeetingReviewService;
+import ua.ukma.nc.service.MeetingService;
+import ua.ukma.nc.service.StudentService;
+import ua.ukma.nc.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.logging.Logger;
 
 /**
  * Created by Alex_Frankiv on 14.11.2016.
@@ -13,9 +25,27 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping (value = "/meeting")
 public class MeetingEvaluationController {
 
-    @RequestMapping (value = "/set_attendance", method = RequestMethod.POST, produces = "application/json")
-    public @ResponseBody String setAttendance(){
+    @Autowired
+    MeetingService meetingService;
 
+    @Autowired
+    StudentService studentService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MeetingReviewService meetingReviewService;
+
+    @RequestMapping (value = "/set_no_attendance", method = RequestMethod.POST)
+    public @ResponseBody String setAttendance(@RequestParam Long studentId, @RequestParam Long meetingId){
+        MeetingReview review = new MeetingReviewImpl();
+        User mentor = userService.getByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        review.setMentor(mentor);
+        review.setMeeting(meetingService.getById(meetingId));
+        review.setStudent(userService.getById(studentId));
+        review.setType("A");
+        meetingReviewService.createMeetingReview(review);
         return "true";
     }
 
